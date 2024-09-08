fb568d7e559dbf252b7fa9cf9bb81f96eee59bd9
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-09-08 12:07:10 +0700

    Soal Praktikum Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,22 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 // Route untuk halaman Home
 Route::get('/', [HomeController::class, 'index']);
 
+Route::get('/', function () {
+    return view('welcome');
+});
 // Route untuk halaman Products
 Route::get('/products', [ProductController::class, 'index']);
 
